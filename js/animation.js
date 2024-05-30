af2ed87da264a17e4f025edcd27bf2f1003b8289
bisharu <bisharu@veuz.in>
2024-05-30 10:52:00 +0530

    2nd push

--- js/animation.js
+++ js/animation.js
@@ -27,3 +27,38 @@ gsap.to("#bird", {
 });
 
 
+
+
+// ------------  window animation----------//
+
+
+
+// Ensure the DOM is fully loaded before running the animation
+document.addEventListener('DOMContentLoaded', () => {
+// Register the ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+// Function to create scroll animation for a given element
+const createScrollAnimation = (element) => {
+    gsap.to(element, {
+        y: '-180px', // Move upwards by 50px
+        duration: 1, // Animation duration of 1 second
+        ease: 'power1.inOut', // Easing function for smooth animation
+        scrollTrigger: {
+            trigger: element, // Element that triggers the animation
+            start: "top center", // Animation starts when the top of the element hits the center of the viewport
+            end: "top 300px", // Animation ends when the top of the element hits 100px from the top of the viewport
+            scrub: true // Smooth scrubbing, the animation follows the scroll
+        }
+    });
+};
+
+// Select elements and create animations
+const view1 = document.querySelector('.view-1');
+const view2 = document.querySelector('.view-2');
+const view3 = document.querySelector('.view-3');
+
+createScrollAnimation(view1);
+createScrollAnimation(view2);
+createScrollAnimation(view3);
+});
